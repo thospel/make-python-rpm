235ec87347de697ccf390210e437c35c4d0e35d4
Ton Hospel <git@ton.iguana.be>
2025-05-15 12:24:19 +0200

    Update to fix complaints from more recent python infrastructure

--- setup.py
+++ setup.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
-# Purely for backward compatibility. All our information is in setup.cfg
-# and modern build systems do not need this script anymore
+"""
+Purely for backward compatibility. All our information is in `setup.cfg`
+and modern build systems do not need this script anymore
+"""
 import setuptools
 
 if __name__ == "__main__":
